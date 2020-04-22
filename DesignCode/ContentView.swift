1548844d6ddfb8362963beb836059c43fef39b2b
David Nguyen <davidnguyen64@outlook.com>
2020-04-22 10:36:27 +0700

    Update ContentView.swift

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -11,16 +11,34 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            BackCardView()
-                .offset(x: 0, y: -40)
-                .offset(x: 0, y: -40)
-                .scaleEffect(0.9)
+            TitleView()
+                .blur(radius: 20)
             
             BackCardView()
+                .background(Color("card4"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: -40)
+                .scaleEffect(0.9)
+                .rotationEffect(.degrees(10))
+                .rotation3DEffect(Angle(degrees: 10), axis: (x: 10.0, y: 0, z: 0))
+                .blendMode(.hardLight)
+            
+            BackCardView()
+                .background(Color("card3"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
                 .offset(x: 0, y: -20)
                 .scaleEffect(0.95)
-
+                .rotationEffect(Angle.degrees(5))
+                .rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0, z: 0))
+                .blendMode(.hardLight)
+            
             CardView()
+                .blendMode(.hardLight)
+            
+            BottomCardView()
+                .blur(radius: 20)
         }
     }
 }
@@ -31,16 +49,16 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct ExtractedView: View {
+struct CardView: View {
     var body: some View {
         VStack {
             HStack {
                 VStack(alignment: .leading) {
-                    Text("David N.")
+                    Text("UI Design")
                         .font(.title)
                         .fontWeight(.semibold)
                         .foregroundColor(.white)
-                    Text("Student")
+                    Text("Certificate")
                         .foregroundColor(Color("accent"))
                 }
                 Spacer()
@@ -66,10 +84,45 @@ struct BackCardView: View {
         VStack {
             Spacer()
         }
-        .frame(width: 300.0, height: 220)
-        .background(Color.blue)
-        .cornerRadius(20)
+        .frame(width: 340, height: 220)
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding()
+            Image("Background1")
+            Spacer()
+        }
+    }
+}
+
+struct BottomCardView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Rectangle()
+                .frame(width: 40, height: 5)
+                .cornerRadius(3)
+                .opacity(0.1)
+            Text("This certificate is proof that Meng To has achieved the UI Design course with approval from a Design+Code instructor.")
+                .multilineTextAlignment(.center)
+                .font(.subheadline)
+                .lineSpacing(4)
+            Spacer()
+        }
+        .padding(.top, 8)
+        .padding(.horizontal, 20)
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
         .shadow(radius: 20)
-        .offset(x: 0, y: -20)
+        .offset(x: 0, y: 500)
     }
 }
